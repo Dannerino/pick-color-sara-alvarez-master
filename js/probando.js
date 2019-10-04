3c5af0445f8ab625183d203cf9ee154e9cfeed44
Dannerino <tarumendentany@gmail.com>
2019-10-04 09:12:04 +0200

    hexadecimalAutocompletado

--- js/probando.js
+++ js/probando.js
@@ -4,10 +4,13 @@ function hex(h) {
     var r = h.charAt(2) + h.charAt(3);
     var g = h.charAt(4) + h.charAt(5);
     var b = h.charAt(6) + h.charAt(7);
-    r = parseInt(r, 16);
-    g = parseInt(g, 16);
-    b = parseInt(b, 16);
-    //if (r > 0 && r < 255 && g > 0 && g < 255 && b > 0 && b < 255)
+    r = parseInt(r, 16).toString();
+    g = parseInt(g, 16).toString();
+    b = parseInt(b, 16).toString();
+    
+    r=r.padStart(3, "0");
+    g=g.padStart(3, "0");
+    b=b.padStart(3, "0");
 
     return r + '' + g + '' + b;
 
